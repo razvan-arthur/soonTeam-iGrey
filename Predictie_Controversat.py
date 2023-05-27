b14a3373b6aa37d8828c144dbc4056a6bfd4d040
Vilceanu <arthur.vilceanu@dell.com>
2023-05-28 01:52:36 +0300

    Merged AI scripts to main app

--- Predictie_Controversat.py
+++ Predictie_Controversat.py
@@ -17,49 +17,53 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 # from sklearn.neural_network import MLPClassifier
 # from sklearn.metrics import accuracy_score
 import joblib
+import os
+def check_controversy(username):
+    userList=[]
+    userList.append(username)
+    current_path = os.getcwd()
+    chromedriver_path = current_path + "/chromedriver"
+    cookies = ""  # no account cookies
+    proxy_list = []
+    twitter_handle = userList
+    scraper = TwitterScraper(twitter_handle, chromedriver_path, cookies, proxy_list)
+    data = scraper.getTweetsText()
 
-chromedriver_path = "C:\soonTeam-iGrey"
-cookies = ""  # no account cookies
-proxy_list = []
-twitter_handle = ["realDonaldTrump"]
-scraper = TwitterScraper(twitter_handle, chromedriver_path, cookies, proxy_list)
-data = scraper.getTweetsText()
+    tweetdata = []
 
-tweetdata = []
+    for entry in data:
+        tweets = entry['tweets']
+        for tweet in tweets:
+            tweetdata.append(tweet['text'])
 
-for entry in data:
-    tweets = entry['tweets']
-    for tweet in tweets:
-        tweetdata.append(tweet['text'])
+    loaded_model = joblib.load('model.pkl')
+    vectorizer = TfidfVectorizer()
 
-loaded_model = joblib.load('model.pkl')
-vectorizer = TfidfVectorizer()
+    # Fit the vectorizer on your training data
+    vectorizer = joblib.load('vectorizer_check.pkl')
 
-# Fit the vectorizer on your training data
-vectorizer = joblib.load('vectorizer_check.pkl')
+    X_test = vectorizer.transform(tweetdata)
 
-X_test = vectorizer.transform(tweetdata)
+    # Make predictions
+    predictions = loaded_model.predict(X_test)
+    pos=0
+    cons=0
+    lista = []
+    for i in predictions:
+        if i == "positive":
+            pos+=1;
+            lista.append(1)
+        if i == "negative":
+            lista.append(-1)
+            cons+=1
 
-# Make predictions
-predictions = loaded_model.predict(X_test)
-pos=0
-cons=0
-lista = []
-for i in predictions:
-    if i == "positive":
-        pos+=1;
-        lista.append(1)
-    if i == "negative":
-        lista.append(-1)
-        cons+=1
+    # indice = np.sum(lista) / len(lista)
+    # print( indice)
+    pred = 100 - (pos/len(predictions) * 100)
 
-indice = np.sum(lista) / len(lista)
-print( indice)
-if (cons!=0):
-    pred= pos/cons
-    print(  pred)
+    return pred
 
-# In[ ]:
+    # In[ ]:
 
 
 
