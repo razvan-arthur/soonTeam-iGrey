2e5de45f308e94268c31dfdcb3fbe0c27be5212f
Vilceanu Razvan-Arthur <60941725+razvan-arthur@users.noreply.github.com>
2023-05-28 05:24:36 +0300

    Create ci-script.py

---
+++ ci-script.py
@@ -0,0 +1,44 @@
+import json
+import git
+import subprocess
+
+# Clone the Git repository
+repo_url = "https://github.com/razvan-arthur/soonTeam-iGrey"  # Replace with your Git repository URL
+repo_dir = "repo"  # Directory where the repository will be cloned
+git.Repo.clone_from(repo_url, repo_dir)
+
+# Load the JSON file
+json_file_path = f"{repo_dir}/query-data.json"  # Replace with the actual path to your JSON file
+with open(json_file_path) as file:
+    data = json.load(file)
+
+# Extract values from JSON
+business_name = data["business-name"]
+stock_name = data["stock-name"]
+domain = data["domain"]
+ceo_username = data["ceo-username"]
+
+# Process values using separate Python scripts
+process_scripts = {
+    "business-name": "Cod_sentimental.py",
+    "stock-name": "checkVolatility.py",
+    "domain": "checktrend.py",
+    "ceo-username": "Predictie_Controversat.py",
+}
+
+processed_data = {}
+for field, script in process_scripts.items():
+    command = ["python3", script, data[field]]
+    result = subprocess.run(command, capture_output=True, text=True)
+    processed_data[field] = result.stdout.strip()
+
+# Save processed data to a new JSON file
+output_file_path = f"{repo_dir}/processed_data.json"
+with open(output_file_path, "w") as file:
+    json.dump(processed_data, file)
+
+# Commit and push the changes to the Git repository
+repo = git.Repo(repo_dir)
+repo.git.add(output_file_path)
+repo.index.commit("Updated processed data")
+repo.remote().push()
